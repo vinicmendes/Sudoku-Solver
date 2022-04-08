91fb9cceae3e7aaac4559df944b553df1ca1f8e2
Vinícius <vinicius.o.mendes@ufv.br>
2022-04-07 23:34:56 -0300

    functions

--- src/src/main/java/src/SudokuSolver.java
+++ src/src/main/java/src/SudokuSolver.java
@@ -1,14 +1,51 @@
-
-
 package src;
 
 /**
  *
  * @author vinicius
  */
-public class Src {
+public class SudokuSolver {
+
+    private static final int GRID_SIZE = 9;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[][] board = {
+            {7, 0, 2, 0, 5, 0, 6, 0, 0},
+            {0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {1, 0, 0, 0, 0, 9, 5, 0, 0},
+            {8, 0, 0, 0, 0, 0, 0, 9, 0},
+            {0, 4, 3, 0, 0, 0, 7, 5, 0},
+            {0, 9, 0, 0, 0, 0, 0, 0, 8},
+            {0, 0, 9, 7, 0, 0, 0, 0, 5},
+            {0, 0, 0, 2, 0, 0, 0, 0, 0},
+            {0, 0, 7, 0, 4, 0, 2, 0, 3}
+        };
+    }
+
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if(board[row][i] == number){
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean isNumberInColumn(int[][] board, int number, int column) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if(board[i][column] == number){
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+        
+        for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                if(board[i][j])
+            }
+        }
     }
 }
